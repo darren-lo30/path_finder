f1c99b4787ec06cf3704eb0f197785abbbe2772a
DarrenLo0530 <darren.lo.0530@gmail.com>
2022-06-08 02:07:36 -0400

    Added map

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:walk_finder/map.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Text('hello world'),);
+    return MapView();
   }
 }
